fbf69a56dcf391cee0d92abde3efe08e22432ce9
LindaDaugule <Bieza777@inbox.lv>
2022-04-20 22:44:37 +0300

    Add project files.

---
+++ HomeWork3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HomeWork3;
+
+
+Telefons telefons = new Telefons();
+Console.WriteLine("Enter size, brand, model");
+string size = Console.ReadLine();
+string brand = Console.ReadLine();
+string model = Console.ReadLine();
+
+telefons.Size = "large";
+telefons.Brand = ;
+telefons.Model = ;
+
+Console.WriteLine($"Apsveicu, Jūs izveidojāt {telefons.Size}");
---
+++ HomeWork3/Telefons.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork3
+{
+    public class Telefons
+    {
+        public double Size { get; set; }
+        public double Brand { get; set; }
+        public double Model { get; set; }
+        public void Call()
+        {
+        Console.WriteLine("Calling");
+        }
+        public void Sms()
+        {
+            Console.WriteLine("Calling");
+        }
+    }
+}
